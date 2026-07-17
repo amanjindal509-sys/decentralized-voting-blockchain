5bb6c70ac518ed88ad4bea08f60591267cda390d
amanjindal509-sys <amanjindal509@gmail.com>
2026-07-17 15:15:59 +0530

    Add files via upload

---
+++ Index.tsx
@@ -0,0 +1,9 @@
+// Update this page (the content is just a fallback if you fail to update the page)
+
+import { VotingApp } from '@/components/VotingApp';
+
+const Index = () => {
+  return <VotingApp />;
+};
+
+export default Index;
